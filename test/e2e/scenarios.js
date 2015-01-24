2b760050ff470bbca661a6a3f5f22d13cd7b5d46
patthsu <patthsu@gmail.com>
2015-01-23 17:51:13 -0800

    Step 4

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,3 +1,7 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
 describe('PhoneCat App', function() {
 
   describe('Phone list view', function() {
@@ -6,10 +10,12 @@ describe('PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
-    var phoneList = element.all(by.repeater('phone in phones'));
-    var query = element(by.model('query'));
 
     it('should filter the phone list as a user types into the search box', function() {
+
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
+
       expect(phoneList.count()).toBe(3);
 
       query.sendKeys('nexus');
@@ -20,12 +26,31 @@ describe('PhoneCat App', function() {
       expect(phoneList.count()).toBe(2);
     });
 
-    it('should display the current filter value in the title bar', function() {
-      query.clear();
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
 
-      query.sendKeys('nexus');
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    it('should be possible to control phone order via the drop down select box', function() {
+
+      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+      var query = element(by.model('query'));
+
+      function getNames() {
+        return phoneNameColumn.map(function(elm) {
+          return elm.getText();
+        });
+      }
+
+      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+      expect(getNames()).toEqual([
+        "Motorola XOOM\u2122 with Wi-Fi",
+        "MOTOROLA XOOM\u2122"
+      ]);
+
+      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+      expect(getNames()).toEqual([
+        "MOTOROLA XOOM\u2122",
+        "Motorola XOOM\u2122 with Wi-Fi"
+      ]);
     });
   });
 });
