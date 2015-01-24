0016ea29df4154665f6ae37104d3c91cabfdb501
patthsu <patthsu@gmail.com>
2015-01-23 17:29:56 -0800

    Step 3

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -11,8 +11,7 @@ describe('PhoneCat controllers', function() {
       var scope = {},
           ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
-      expect(scope.phones.length).toBe(4);
-      expect(scope.name).toBe('World');
+      expect(scope.phones.length).toBe(3);
     }));
 
   });
